5e007af5bbcee90365f6e4e2554494ac8f2136f2
kevin2802 <kah9968@nyu.edu>
2024-05-28 16:56:21 -0400

    userschema created, routes added

--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.js'
+//this is where the server lives
 
+//allow dotenv to work
 dotenv.config();
+
+//connect to mongoose using dotenv variable
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('connected to mongo')
 })
 
+//create server on port 3000
 const app = express();
 app.listen(3000,()=>{
     console.log("SERVER RUNNING ON 3000!")
 })
+
+//routes could live here (app.get/app.post) or in their own files
+
+app.use("/server/user",userRoutes);
---
+++ server/models/user.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+// a mongoose schema for users on this website
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        
+    },
+    password:{
+        type:String,
+        required:true,
+
+        
+    }
+},
+{timestamps:true});
+const User = mongoose.model("User",userSchema);
+export default User;
