f14df50a95b69fdbd74e65c32cc1eeb4b2450bcc
kevin2802 <kah9968@nyu.edu>
2024-06-03 22:43:16 -0400

    create signin server route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
 
 export const signup = async (req,res,next)=>{
@@ -15,4 +16,32 @@ export const signup = async (req,res,next)=>{
         next(error)
     }
     
+}
+export const signin = async(req,res,next)=>{
+    const{email,password}=req.body;
+    try{
+        const validUser = await User.findOne({email});//search for email using mongoose
+        if(!validUser){//if email doesnt exist in db throw error
+            return next(errorHandler(401,"invalid credentials"));
+        }
+        const validPassword = bcryptjs.compareSync(password,validUser.password);
+        //if not valid password
+        if(!validPassword){
+            return next(errorHandler(401,'wrong email/password'));
+        }
+        //JWT is unique to this project
+        const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+        //need to remove password from being sent to frontend
+        //do this with spread operator
+        const{password:hashedpw,...rest}=validUser._doc;
+        //._doc removes unnecesary stuff
+        //could add a date object and use expires to not have to relogin everytime(i.e expire 1 hour from now)
+        //could delete and have to relogin everytime
+        const expireDate = new Date(Date.now()+360000)//1 hour from now
+
+        res.cookies('accesstoken',token,{ httpOnly:true ,expires:expireDate}).status(200).json(rest);
+    }
+    catch(error){
+        next(error);
+    }
 }
