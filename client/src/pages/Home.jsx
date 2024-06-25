2c627a65f8e85b2e351eb2f2f6d23ce71ffcccf4
kevin2802 <kah9968@nyu.edu>
2024-06-25 01:46:10 -0400

    css

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux'
 import { SVGMap } from 'react-svg-map'
 import USACounties from '@svg-maps/usa.counties'
 import USA from '@svg-maps/usa'
+import './styles.css';
 export default function Home() {
   const {currentUser }= useSelector((state)=>state.user)
 
@@ -12,13 +13,13 @@ export default function Home() {
       <div>
         <h1 className='text-3xl text-center font-semibold my-7'>Welcome to County-Tracker!</h1>
       </div>
-      <div className='flex gap-4'>
+      <div className='flex flex-row gap-4 justify-center'>
         <Link to='/county-map'>
-        <SVGMap map={USACounties}/>
+        <SVGMap map={USACounties}locationClassName={'home'}/>
         <p className='text-center'>Track Counties</p>
         </Link>
         <Link to='/states-map'>
-        <SVGMap map={USA}/>
+        <SVGMap map={USA}locationClassName={'home'}/>
         <p className='text-center'>Track states</p>
         </Link>
       </div>
